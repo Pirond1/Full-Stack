aa7f14ba8212a4e94d60fbae375035d227960e01
Gustavo Pirondi <gustavopirondi@gmail.com>
2024-08-05 22:17:44 -0300

    Projeto de Revisão
    Início do Projeto de Revisão da aula

---
+++ ProjetoDeRevisão/Entidades/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Pessoa
+    {
+        public int codigo { get; set; }
+        public string nome { get; set; } = string.Empty;
+        public DateTime dataNascimento { get; set; }
+        public string cpf { get; set; } = string.Empty;
+        public string rg { get; set; } = string.Empty;
+        public string endereco { get; set; } = string.Empty;
+
+        public int CalcularIdade()
+        {
+            int idade = DateTime.Now.Year - dataNascimento.Year;
+            if (dataNascimento.Month > DateTime.Now.Month)
+            {
+                idade = idade - 1;
+            }
+            return idade;
+        }
+    }
+}
